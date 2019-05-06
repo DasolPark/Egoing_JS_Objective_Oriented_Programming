426c31369c55fb81386b373ded75b7d78dcf221b
Dasol Park <dasol.park615@gmail.com>
2019-05-06 22:05:35 +0800

    8. Make a method of an object in class

--- class.js
+++ class.js
@@ -4,8 +4,30 @@ class Person {
     this.math = math;
     this.english = english;
   }
+  // 1. method in class (recomened method)
+  sum() {
+    return "original: " + (this.math + this.english);
+  }
 }
 
-var dasol = new Person("david", 90, 99);
+// 2. prototype
+// Person.prototype.sum = function() {
+//   return "prototype: " + (this.math + this.english);
+// };
 
-console.log("DASOL", dasol);
+var dasol = new Person("david", 90, 99);
+var hyunjin = new Person("judy", 99, 95);
+// 3. personalize method(method overriding)
+hyunjin.sum = function() {
+  return "hyunjin sum: " + (this.math + this.english);
+};
+
+console.group("DASOL");
+console.log("DASOL: ", dasol);
+console.log("DASOL SUM: ", dasol.sum());
+console.groupEnd("DASOL END");
+
+console.group("HYUNJIN");
+console.log("HYUNJIN: ", hyunjin);
+console.log("HYUNJIN SUM: ", hyunjin.sum());
+console.groupEnd("HYUNJIN END");
