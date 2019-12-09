c52453f053bf23ca75992417064e4a0c1a408ae6
Dasol Park <dasol.park615@gmail.com>
2019-12-09 15:53:54 +0900

    What is closure?

--- closure.js
+++ closure.js
@@ -1,8 +1,18 @@
+// function outer() {
+//   var title = 'hi';
+//   function inner() {
+//     console.log(title);
+//   }
+//   inner();
+// }
+// outer();
+
 function outer() {
   var title = 'hi';
-  function inner() {
+  return function() {
     console.log(title);
-  }
-  inner();
+  };
 }
-outer();
+
+inner = outer();
+inner();
