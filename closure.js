af3c9a633ab88dd7406b1eb9cbd899517f352762
Dasol Park <dasol.park615@gmail.com>
2019-12-09 16:37:54 +0900

    Application of closure

--- closure.js
+++ closure.js
@@ -23,29 +23,59 @@
 
 // 3. Private variable
 
-function factory_movie(title) {
-  return {
-    get_title: function() {
-      return title;
-    },
-    set_title: function(_title) {
-      title = _title;
-      //if(typeof _title === 'String'){
-      //   title = _title;
-      // }else{
-      //   console.log('Please Input String');
-      // }
-    }
-  };
+// function factory_movie(title) {
+//   return {
+//     get_title: function() {
+//       return title;
+//     },
+//     set_title: function(_title) {
+//       title = _title;
+//       //if(typeof _title === 'String'){
+//       //   title = _title;
+//       // }else{
+//       //   console.log('Please Input String');
+//       // }
+//     }
+//   };
+// }
+
+// ghost = factory_movie('Ghost in the shell');
+// matrix = factory_movie('Matrix');
+
+// console.log(ghost.get_title());
+// console.log(matrix.get_title());
+
+// ghost.set_title('Inception');
+
+// console.log(ghost.get_title());
+// console.log(matrix.get_title());
+
+// 4. application of closure
+
+// X
+// var arr = [];
+// for (var i = 0; i < 5; i++) {
+//   arr[i] = function() {
+//     return i;
+//   };
+// }
+// for (var index in arr) {
+//   console.log(arr[index]());
+// }
+// for의 i는 외부함수의 지역변수가 아니다.
+
+// O
+var arr = [];
+for (var i = 0; i < 5; i++) {
+  arr[i] = (function(id) {
+    return function() {
+      return id;
+    };
+  })(i);
+}
+for (var index in arr) {
+  console.log(arr[index]());
 }
 
-ghost = factory_movie('Ghost in the shell');
-matrix = factory_movie('Matrix');
-
-console.log(ghost.get_title());
-console.log(matrix.get_title());
-
-ghost.set_title('Inception');
-
-console.log(ghost.get_title());
-console.log(matrix.get_title());
+// var -> function scope
+// let, const -> block scope
