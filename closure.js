38769f412c551db668fa02d0c4a937454db60946
Dasol Park <dasol.park615@gmail.com>
2019-12-09 16:15:55 +0900

    Private variable

--- closure.js
+++ closure.js
@@ -1,3 +1,5 @@
+// 1. inner & outer function
+
 // function outer() {
 //   var title = 'hi';
 //   function inner() {
@@ -7,12 +9,43 @@
 // }
 // outer();
 
-function outer() {
-  var title = 'hi';
-  return function() {
-    console.log(title);
+// 2. What is closure?
+
+// function outer() {
+//   var title = 'hi';
+//   return function() {
+//     console.log(title);
+//   };
+// }
+
+// inner = outer();
+// inner();
+
+// 3. Private variable
+
+function factory_movie(title) {
+  return {
+    get_title: function() {
+      return title;
+    },
+    set_title: function(_title) {
+      title = _title;
+      //if(typeof _title === 'String'){
+      //   title = _title;
+      // }else{
+      //   console.log('Please Input String');
+      // }
+    }
   };
 }
 
-inner = outer();
-inner();
+ghost = factory_movie('Ghost in the shell');
+matrix = factory_movie('Matrix');
+
+console.log(ghost.get_title());
+console.log(matrix.get_title());
+
+ghost.set_title('Inception');
+
+console.log(ghost.get_title());
+console.log(matrix.get_title());
